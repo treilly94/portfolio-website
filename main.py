9b2e781950ab61e7bc790666f3b1b53d033e38b9
Tom Reilly <johndoe@example.com>
2017-10-02 16:11:18 +0100

    added exp, project, and tech pages

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/tech')
+def tech():
+    return render_template('tech.html')
+
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+
+@app.route('/exp')
+def exp():
+    return render_template('exp.html')
+
+
 if __name__ == '__main__':
     app.run()
